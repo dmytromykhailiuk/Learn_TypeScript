11154b60fffa5a4a83161f37476a1d8f2247ff06
dmytromykhayluk <52799910+dmytromykhayluk@users.noreply.github.com>
2020-04-02 12:50:41 +0300

    advanced_typing_concepts

--- app.ts
+++ app.ts
@@ -1,16 +1,2 @@
 
-//-----------------------inersection----------------------------------
 
-
-type Named1 = {
-  name: string
-}
-
-type Aged1 = {
-  age: number
-}
-
-type Combinable = boolean | number
-
-// type Universal = Named1 | Combinable   
-type Universal = Named1 & Combinable  // same result
